58359711e8eb91828812561c34de6c95cf9353a8
ThomasBakaysaJr <thomasbakaysajr@gmail.com>
2025-11-25 02:22:50 -0600

    refactor: now creates invidual log records for tests

--- utils/record_total_time.py
+++ utils/record_total_time.py
@@ -1,21 +1,29 @@
 import time
+import datetime
 import json
+import os
 
-def record_total_time(total_time, config, output_file="total_time_log.json"):
+def record_total_time(total_time, config, output_suffix="total_times_log.json"):
+    '''
+    Create a running record of the total time taken for test runs 
+    along with their configuration(s) to a JSON file.
+    Parameters:
+        total_time (float): The total time taken for the test run in seconds.
+        config (dict): The configuration dictionary used for the test run.
+            Can be a single config or a list of configs.
+        output_file (str): The path to the JSON file where the log will be stored.'''
+    os.makedirs("data", exist_ok=True)
+    filename = datetime.datetime.now().strftime(f"data/%Y-%m-%d_{output_suffix}")
+
     log_entry = {
-        "timestamp": time.time(),
+        'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "total_time": total_time,
         "config": config
     }
     
     # Append the log entry to the JSON file
-    try:
-        with open(output_file, 'r') as f:
-            data = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        data = []
-    
-    data.append(log_entry)
-    
-    with open(output_file, 'w') as f:
-        json.dump(data, f, indent=4)
+    with open(filename, 'a') as f:
+        # Write the line + a newline character
+        f.write(json.dumps(log_entry) + "\n")
+
+    print(f'Recorded total time: {total_time} seconds to {filename}')
